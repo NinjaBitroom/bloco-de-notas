6f38546295d75fdf456758ed077b0f6facac6286
Ladraozito <gabrielbitercourt@gmail.com>
2019-09-03 23:13:55 -0400

    janela de salvar movida para um arquivo separado

---
+++ blocoanotavel/janelasalvar.py
@@ -0,0 +1,45 @@
+from tkinter import Label, NSEW, Button, W, EW, E
+
+
+class JanelaSalvar:
+    def __init__(self, master):
+        self.mestre = master
+        master.janelaDeSalvar.geometry('250x100+500+500')
+        master.janelaDeSalvar.title(master.diretorio)
+        master.janelaDeSalvar.grid_columnconfigure(0, weight=1)
+        master.janelaDeSalvar.grid_columnconfigure(1, weight=1)
+        master.janelaDeSalvar.grid_columnconfigure(2, weight=1)
+        master.janelaDeSalvar.grid_rowconfigure(0, weight=3)
+        master.janelaDeSalvar.grid_rowconfigure(1, weight=1)
+
+        pergunta = Label(master.janelaDeSalvar, text='Deseja salvar?')
+        pergunta.grid(column=0, row=0, columnspan=3, sticky=NSEW)
+        pergunta['font'] = ('Arial', 20)
+
+        botaoSalvar = Button(master.janelaDeSalvar, text='Salvar', width=6, command=self.sairSalvando)
+        botaoSalvar.grid(column=0, row=1, sticky=W)
+
+        botaoNaoSalvar = Button(master.janelaDeSalvar, text='Não salvar', width=6, command=self.sairSemSalvar)
+        botaoNaoSalvar.grid(column=1, row=1, sticky=EW)
+
+        botaoCancelar = Button(master.janelaDeSalvar, text='Cancelar', width=6, command=self.cancelar)
+        botaoCancelar.grid(column=2, row=1, sticky=E)
+
+        master.janelaDeSalvar.resizable(False, False)
+        master.janelaDeSalvar.transient(master.atual)
+        master.janelaDeSalvar.focus_force()
+        master.janelaDeSalvar.grab_set()
+        master.janelaDeSalvar.protocol('WM_DELETE_WINDOW', self.cancelar)
+
+    def sairSalvando(self):
+        self.mestre.salvar()
+        self.mestre.janelaDeSalvar.destroy()
+        self.mestre.atual.destroy()
+
+    def sairSemSalvar(self):
+        self.mestre.janelaDeSalvar.destroy()
+        self.mestre.atual.destroy()
+
+    def cancelar(self):
+        self.mestre.janelaDeSalvar.destroy()
+        self.mestre.janelaDeSalvar = None
